1d3e06de03796afb4e3bb212cb3b41e3c2fa42e3
murilonicemento <murilo_nascimento22@outlook.com>
2023-06-09 13:59:33 -0300

    Finalização Projeto

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,16 @@
-const toggleTheme = document.getElementById("toggle-theme") as HTMLInputElement;
-const imageToggle = document.getElementById("handle-icon") as HTMLImageElement;
+const toggleTheme = document.querySelector("#switch button") as HTMLDivElement;
 
 toggleTheme.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme");
   document.body.classList.toggle("light-theme");
 
   if (document.body.classList.contains("dark-theme")) {
-    imageToggle.setAttribute("src", "assets/images/handle-dark.svg");
     handleIcon("linkedin", "dark");
     handleIcon("github", "dark");
     handleIcon("hackerrank", "dark");
     handleIcon("instagram", "dark");
     handleIcon("twitter", "dark");
   } else {
-    imageToggle.setAttribute("src", "assets/images/handle-light.svg");
     handleIcon("linkedin", "light");
     handleIcon("github", "light");
     handleIcon("hackerrank", "light");
