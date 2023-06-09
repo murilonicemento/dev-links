d86323aa9fbb2de0f79875f6fea36ccd7a15fc7c
murilonicemento <murilo_nascimento22@outlook.com>
2023-06-09 13:33:37 -0300

    Finalização da lógica de mudança de tema

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,22 @@ toggleTheme.addEventListener("click", () => {
 
   if (document.body.classList.contains("dark-theme")) {
     imageToggle.setAttribute("src", "assets/images/handle-dark.svg");
+    handleIcon("linkedin", "dark");
+    handleIcon("github", "dark");
+    handleIcon("hackerrank", "dark");
+    handleIcon("instagram", "dark");
+    handleIcon("twitter", "dark");
   } else {
     imageToggle.setAttribute("src", "assets/images/handle-light.svg");
+    handleIcon("linkedin", "light");
+    handleIcon("github", "light");
+    handleIcon("hackerrank", "light");
+    handleIcon("instagram", "light");
+    handleIcon("twitter", "light");
   }
 });
+
+function handleIcon(icon: string, theme: string): void {
+  const image = document.getElementById(icon) as HTMLImageElement;
+  image.setAttribute("src", `assets/images/${theme}-theme-${icon}.svg`);
+}
