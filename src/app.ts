520b7de33c4085b8b8324ffe0c167dd8530d706e
murilonicemento <murilo_nascimento22@outlook.com>
2023-06-09 13:07:17 -0300

    Inclusão da lógica para mudança de tema

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("oi");
+const toggleTheme = document.getElementById("toggle-theme") as HTMLInputElement;
+const imageToggle = document.getElementById("handle-icon") as HTMLImageElement;
+
+toggleTheme.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  document.body.classList.toggle("light-theme");
+
+  if (document.body.classList.contains("dark-theme")) {
+    imageToggle.setAttribute("src", "assets/images/handle-dark.svg");
+  } else {
+    imageToggle.setAttribute("src", "assets/images/handle-light.svg");
+  }
+});
